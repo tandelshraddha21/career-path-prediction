954dd2f3074c7bf83b365b44eeafdf2fc684944e
LokeshPatil-loki <lokesh1404@outlook.com>
2024-04-16 23:50:49 +0530

    updated aptitude quiz

--- client/src/screens/AptitudeQuiz.jsx
+++ client/src/screens/AptitudeQuiz.jsx
@@ -28,35 +28,33 @@ function AptitudeQuiz() {
   const handleOptionChange = (event) => {
     console.log(event.target);
     setSelectedOption(event.target.value);
+    setQuestions((prev) => {
+      return prev.map((q, index) => {
+        if (index == currentQuestion) {
+          q.selectedOption = event.target.value;
+        }
+        return q;
+      });
+    });
   };
 
-  const checkAnswer = async () => {
-    if (selectedOption.length === 0) {
-      alert("Please select an option");
-      return;
-    }
-    console.log(question.question_id);
-    const response = await fetch("http://127.0.0.1:5000/check_answer", {
+  const check_answers = async () => {
+    const data = {
+      answers: questions.map((item) => {
+        return {
+          question_id: item.question_id,
+          user_answer: item.selectedOption,
+        };
+      }),
+      category: "aptitude",
+    };
+    const response = await fetch("http://127.0.0.1:5000/check_answers", {
       method: "POST",
       headers: { "Content-type": "application/json" },
-      body: JSON.stringify({
-        question_id: question.question_id,
-        user_answer: selectedOption,
-        category: category,
-      }),
+      body: JSON.stringify(data),
     });
-
-    const result = await response.json();
-    if (result.result === "correct") {
-      setScore(score + 1);
-    }
-    if (currentQuestion === 9) {
-      updateAptitudeScore(score);
-      navigate("/quiz/coding");
-    } else {
-      setCurrentQuestion(currentQuestion + 1);
-    }
-    setTime(10);
+    const score = await response.json();
+    updateAptitudeScore(score.score);
   };
 
   const question = questions[currentQuestion];
@@ -72,9 +70,7 @@ function AptitudeQuiz() {
         id={"apti"}
         initialTime={600}
         onTimeUp={() => {
-          //   setCurrentQuestion(currentQuestion + 1);
-          updateAptitudeScore(score);
-          navigate("/quiz/coding");
+          check_answers();
         }}
       />
       <div className="flex flex-col bg-slate-100 w-full rounded-xl p-10 items-start">
@@ -83,29 +79,23 @@ function AptitudeQuiz() {
           {question.questions}
         </p>
         <Options
-          selectedOption={selectedOption}
+          selectedOption={
+            questions[currentQuestion]?.selectedOption || selectedOption
+          }
           handleOptionChange={handleOptionChange}
           options={[question.a, question.b, question.c, question.d]}
         />
         <button
-          onClick={checkAnswer}
+          onClick={
+            currentQuestion == 9
+              ? check_answers
+              : (e) => setCurrentQuestion(currentQuestion + 1)
+          }
           className="bg-purple-800  text-white p-4 pl-8 pr-8 rounded-xl text-xl"
         >
           {currentQuestion == 9 ? "Finish" : "Next"}
         </button>
       </div>
-      {/* <div className="w-full flex flex-row flex-wrap gap-3 items-center justify-center">
-        {questions.map((question, index) => (
-          <div
-            onClick={(e) => setCurrentQuestion(index)}
-            className={`cursor-pointer border border-1 border-purple-500 w-10 h-10 p-2 ${
-              currentQuestion == index && "bg-purple-800 text-white"
-            }`}
-          >
-            {index + 1}
-          </div>
-        ))}
-      </div> */}
       <NavigateQuestions
         questions={questions}
         currentQuestion={currentQuestion}
